b0803da6a20529f97beccd34e00445edafa11509
sonntaghimself <samuel@sonntag.tv>
2024-10-22 07:56:30 +0200

    Deleting the Test Rep

--- JavaScript/RedTest/RequiredFiles/Common7+/password.php
+++
@@ -1,7 +0,0 @@
-<?php
-function check_password($psw)
-{
-    return strcmp($psw, "");
-}
-$result = check_password($_POST["password"]);
-echo $result;
--- JavaScript/RedTest/RequiredFiles/Common7+/write_code.php
+++
@@ -1,5 +0,0 @@
-<?php
-$name = $_SERVER['DOCUMENT_ROOT'].$_POST['filename'].".csv";
-$data = $_POST['filedata'];
-file_put_contents($name, $data.PHP_EOL, FILE_APPEND);
-?>
